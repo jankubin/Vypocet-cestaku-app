b856748843557468810586896b42609ad9c0b96e
Jan Kubín <jan.kubin1@icloud.com>
2022-08-17 21:40:05 +0200

    Initial Commit

---
+++ Výpočet Cesťáků/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Výpočet Cesťáků
+//
+//  Created by Jan Kubín on 17.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
